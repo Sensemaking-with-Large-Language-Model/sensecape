c93fb0e5361a524311c65d9f212e0cb633baa382
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2023-02-20 11:58:31 -0800

    Completed memo node styleing

--- src/app/nodes/memo-node/memo-node.tsx
+++ src/app/nodes/memo-node/memo-node.tsx
@@ -1,16 +1,55 @@
-import { useState } from "react";
-import { NodeProps } from "reactflow";
+import { useCallback, useState } from "react";
+import { NodeProps, useStore } from "reactflow";
+import { getGPT3Keywords } from "../../../api/openai-api";
 import { ReactComponent as DragHandle } from '../../assets/drag-handle.svg';
+import { ZoomState } from "../node.model";
 import './memo-node.scss';
 
+const zoomSelector = (s: any) => s.transform[2];
+
 const MemoNode = (props: NodeProps) => {
+  const [title, setTitle] = useState('Memo Node');
   const [memo, setMemo] = useState('');
+  const [hasUpdated, setHasUpdated] = useState(false);
+  const zoom: number = useStore(zoomSelector);
+
+  const handleChange = (event: any) => {
+    setMemo(event.target.value);
+    setHasUpdated(true);
+  }
+
+  const generateTitle = useCallback(() => {
+    if (hasUpdated) {
+      getGPT3Keywords(memo).then(data => {
+        setTitle(data ?? 'Memo Node');
+        setHasUpdated(false);
+      });
+    };
+  },
+  [hasUpdated]);
 
   return (
     <div className="memo-node">
-      <DragHandle className='drag-handle' />
-      <h3>Memo</h3>
-      <textarea></textarea>
+      {
+        zoom >= ZoomState.SUMMARY ?
+          (<>
+            <div className="drag-handle">
+              <DragHandle className='drag-handle' />
+              <h3>{title}</h3>
+            </div>
+            <textarea
+              value={memo}
+              onChange={handleChange}
+              onBlur={generateTitle}
+              placeholder="Type notes here"
+            ></textarea>
+          </>) :
+          (<>
+            <div className="summary drag-handle">
+              <h2>{title}</h2>
+            </div>
+          </>)
+      }
     </div>
   )
 }
