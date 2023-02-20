1cd74cf0b094352ce131a239f1d0af36c91cf328
Sangho Suh <sh31659@gmail.com>
2023-02-20 03:18:53 -0800

    Didn't address the issue of moving the viewport to newly generated response but saving tried apporaches

--- src/app/nodes/chat-node/chat-node.helper.ts
+++ src/app/nodes/chat-node/chat-node.helper.ts
@@ -42,6 +42,11 @@ export const createChatNode = (reactFlowInstance: ReactFlowInstance, sourceId: s
     // change view to show next answer 
     // currently, it focuses on the entire view, might want to move to just recently created node 
     // need to use 'nodes' param in fitViewOptions, cf. https://reactflow.dev/docs/api/types/#fitviewsoptions
-    reactFlowInstance!.fitView({ duration: 900, padding: 0.3 }); 
+    const fitViewOptions = { duration: 900, padding: 0.3, nodes: [{ id: newNode.id}] }
+    // reactFlowInstance!.fitView(fitViewOptions);
+    // reactFlowInstance!.zoomOut( { duration: 1300 } );
+    // setTimeout(() => {
+    //   reactFlowInstance!.setCenter(position.x, position.y, { duration: 2000 });
+    // },1000);
   }, 0);
 }
