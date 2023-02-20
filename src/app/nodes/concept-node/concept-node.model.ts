8b151cd31147f12e192056fa6714fd740bbdf42a
Sangho Suh <sh31659@gmail.com>
2023-02-19 16:59:36 -0800

    Make topicsnodes[] optional

--- src/app/nodes/concept-node/concept-node.model.ts
+++ src/app/nodes/concept-node/concept-node.model.ts
@@ -2,18 +2,19 @@ import { Node } from 'reactflow';
 import { TypeTopicNode } from '../topic-node/topic-node.model';
 
 export interface ConceptNodeData {
-  topicNodes: TypeTopicNode[]
+  topicNodes?: TypeTopicNode[] // optional, since we also have concept node not generated from topics
 }
 
 export type TypeConceptNode = Node<ConceptNodeData>;
-
-export interface ExtendedConceptNodeData {
-  
-}
-
-export type TypeExtendedConceptNode = Node<ConceptNodeData>;
 
 export enum ConceptNodeState {
   INPUT = 'input',
   COMPLETE = 'complete',
 }
+
+
+export interface ExtendedConceptNodeData {
+  
+}
+
+export type TypeExtendedConceptNode = Node<ExtendedConceptNodeData>;
