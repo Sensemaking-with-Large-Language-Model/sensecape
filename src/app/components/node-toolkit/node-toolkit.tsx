538db665ea10d39039883da68cb84d8c8fded4e3
Sangho Suh <sh31659@gmail.com>
2023-03-03 23:28:09 -0800

    Add ability to add GIF or YouTube video to tooltip and style each tooltip differently depending on needs (e.g., wider tooltip for GIF or video)

--- src/app/components/node-toolkit/node-toolkit.tsx
+++ src/app/components/node-toolkit/node-toolkit.tsx
@@ -9,7 +9,31 @@ import './node-toolkit.scss';
 import 'intro.js/introjs.css';
 import { Steps, Hints } from 'intro.js-react';
 
+
+
+const ImageElement = () => {
+    return (
+      <>
+      <h3>Tutorial</h3>
+      <img src="https://i.giphy.com/media/ujUdrdpX7Ok5W/giphy.webp" alt="" />
+      </>
+    )
+}
+
+const VideoElement = () => {
+  return (
+    <>
+    <h3>Tutorial</h3>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/BzSMLP5KpjM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    </>
+  )
+}
+
 // https://github.com/HiDeoo/intro.js-react#introjs-options
+// element - target element
+// intro - content
+// tooltipClass - css for tooltip
+// position - position of tooltip
 const steps = [
   // {
   //   // element: document.querySelector('.reactflow-wrapper') as HTMLElement,
@@ -22,7 +46,16 @@ const steps = [
   {
     element: '.semantic-route',
     intro: 'Semantic route<br><br>[Description]',
+    tooltipClass: 'semantic-route-tooltip',
     position: 'right', 
+  },
+  {
+    intro: <ImageElement/>,
+    tooltipClass: 'gif',
+  },
+  {
+    intro: <VideoElement/>,
+    tooltipClass: 'video',
   },
   {
     element: '.node-toolkit',
@@ -32,34 +65,46 @@ const steps = [
     // highlightClass: 'myHighlightClass',
   },
   {
+    element: '.toolkit-brainstorm',
+    intro: <ImageElement/>,
+    position: 'right',
+    tooltipClass: 'gif',
+  },
+  {
+    element: '.toolkit-search',
+    intro: <ImageElement/>,
+    position: 'right',
+    tooltipClass: 'gif',
+  },
+  {
+    element: '.toolkit-concept',
+    intro: <ImageElement/>,
+    position: 'right',
+    tooltipClass: 'gif',
+  },
+  {
+    element: '.toolkit-memo',
+    intro: <ImageElement/>,
+    position: 'right',
+    tooltipClass: 'gif',
+  },
+  {
+    element: '.toolkit-help',
+    intro: <ImageElement/>,
+    position: 'right',
+    tooltipClass: 'gif',
+  },
+  {
+    element: '.toolkit-reset-canvas',
+    intro: <ImageElement/>,
+    position: 'right',
+    tooltipClass: 'gif',
+  },
+  {
     element: '.zoom-slider',
-    intro: 'Zoom Slider<br><br>[Description]',
+    intro: <ImageElement/>,
     position: 'left',
-  },
-  {
-    element: '.brainstorm-node',
-    intro: 'Brainstorm node<br><br>[Description]',
-    position: 'left',
-  },
-  {
-    element: '.flex-node',
-    intro: 'Flex node<br><br>[Description]',
-    position: 'left',
-  },
-  {
-    element: '.chat-node',
-    intro: 'Search node<br><br>[Description]',
-    position: 'left',
-  },
-  {
-    element: '.concept-node',
-    intro: 'Concept hierarchy node<br><br>[Description]',
-    position: 'left',
-  },
-  {
-    element: '.memo-node',
-    intro: 'Memo node<br><br>[Description]',
-    position: 'left',
+    tooltipClass: 'gif',
   },
   {
     element: '.minimap',
@@ -68,12 +113,14 @@ const steps = [
   },
 ];
 
+
 const introOptions = {
   exitOnEsc: true,
   showStepNumbers: true,
   hidePrev: true,
   hideNext: true,
   showButtons: true,
+  showBullets: false,
   keyboardNavigation: true,
   showProgress: true,
 }
@@ -165,7 +212,7 @@ const NodeToolkit = (props: any) => {
         <Switch checked={props.travellerMode} onChange={props.toggleTravellerMode} size='small' />
         Traveller Mode
       </div>
-      <div className='toolkit-option add-node' draggable onDragStart={(e) => onDragStart(e, 'brainstorm')}>
+      <div className='toolkit-option toolkit-brainstorm add-node' draggable onDragStart={(e) => onDragStart(e, 'brainstorm')}>
         {/* <svg width="16" height="30" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1 8L15 8M8 15L8 1" stroke="#AAA" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg> */}
@@ -177,7 +224,7 @@ const NodeToolkit = (props: any) => {
         </svg>
         Brainstorm
       </div>
-      <div className='toolkit-option add-node' draggable onDragStart={(e) => onDragStart(e, 'chat')}>
+      <div className='toolkit-option toolkit-search add-node' draggable onDragStart={(e) => onDragStart(e, 'chat')}>
         {/* <svg width="16" height="30" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1 8L15 8M8 15L8 1" stroke="#AAA" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg> */}
@@ -189,7 +236,7 @@ const NodeToolkit = (props: any) => {
         </svg>
         Search
       </div>
-      <div className='toolkit-option add-node' draggable onDragStart={(e) => onDragStart(e, 'concept')}>
+      <div className='toolkit-option toolkit-concept add-node' draggable onDragStart={(e) => onDragStart(e, 'concept')}>
         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M10 20C10 21.1046 10.8954 22 12 22C13.1046 22 14 21.1046 14 20C14 18.8954 13.1046 18 12 18C10.8954 18 10 18.8954 10 20Z" stroke="#AAAAAA" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
         <path d="M18 20C18 21.1046 18.8954 22 20 22C21.1046 22 22 21.1046 22 20C22 18.8954 21.1046 18 20 18C18.8954 18 18 18.8954 18 20Z" stroke="#AAAAAA" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
@@ -203,14 +250,14 @@ const NodeToolkit = (props: any) => {
         </svg> */}
         Hierarchy
       </div>
-      <div className='toolkit-option add-node' draggable onDragStart={(e) => onDragStart(e, 'memo')}>
+      <div className='toolkit-option toolkit-memo add-node' draggable onDragStart={(e) => onDragStart(e, 'memo')}>
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M6 1.5V3.75M12 1.5V3.75M5.25 8.25H11.25M5.25 11.25H9M11.25 16.5H6.75C3 16.5 2.25 14.955 2.25 11.865V7.2375C2.25 3.7125 3.5025 2.7675 6 2.625H12C14.4975 2.76 15.75 3.7125 15.75 7.2375V12" stroke="#AAAAAA" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
         <path d="M15.75 12L11.25 16.5V14.25C11.25 12.75 12 12 13.5 12H15.75Z" stroke="#AAAAAA" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
         Memo
       </div>
-      <div className='toolkit-option add-node' draggable onClick={startIntroJS}>
+      <div className='toolkit-option toolkit-help add-node' draggable onClick={startIntroJS}>
         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M17 18.4301H13L8.54999 21.39C7.88999 21.83 7 21.3601 7 20.5601V18.4301C4 18.4301 2 16.4301 2 13.4301V7.42999C2 4.42999 4 2.42999 7 2.42999H17C20 2.42999 22 4.42999 22 7.42999V13.4301C22 16.4301 20 18.4301 17 18.4301Z" stroke="#AAAAAA" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
         <path d="M12.0001 11.36V11.15C12.0001 10.47 12.4201 10.11 12.8401 9.82001C13.2501 9.54001 13.66 9.18002 13.66 8.52002C13.66 7.60002 12.9201 6.85999 12.0001 6.85999C11.0801 6.85999 10.3401 7.60002 10.3401 8.52002" stroke="#AAAAAA" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
@@ -218,7 +265,7 @@ const NodeToolkit = (props: any) => {
         </svg>
         Help
       </div>
-      <Button onClick={clearCanvas} type="text" block danger>Reset Canvas</Button>
+      <Button onClick={clearCanvas} className="toolkit-reset-canvas" type="text" block danger>Reset Canvas</Button>
       <Steps
         enabled={stepsEnabled}
         steps={steps}
