24b8ed6fba7c272b8016463c1e9e959f7d222e5a
Sangho Suh <sh31659@gmail.com>
2023-03-03 21:04:42 -0800

    Add introjs library for onboarding tutorial and trigger it when 'help' button is pressed in node toolkit

--- src/app/components/node-toolkit/node-toolkit.tsx
+++ src/app/components/node-toolkit/node-toolkit.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useReactFlow } from 'reactflow';
 import { Button, Switch } from 'antd';
 import { createChatNode } from '../../nodes/chat-node/chat-node.helper';
@@ -6,10 +6,82 @@ import { ChatNodeData } from '../../nodes/chat-node/chat-node.model';
 import { ConceptNodeData } from '../../nodes/concept-node/concept-node.model';
 import { CreativeNodeData } from '../../nodes/node.model';
 import './node-toolkit.scss';
+import 'intro.js/introjs.css';
+import { Steps, Hints } from 'intro.js-react';
+
+// https://github.com/HiDeoo/intro.js-react#introjs-options
+const steps = [
+  // {
+  //   // element: document.querySelector('.reactflow-wrapper') as HTMLElement,
+  //   element: '.react-flow',
+  //   intro: 'canvas',
+  //   position: 'right',
+  //   // tooltipClass: 'myTooltipClass',
+  //   // highlightClass: 'myHighlightClass',
+  // },
+  {
+    element: '.semantic-route',
+    intro: 'Semantic route<br><br>[Description]',
+    position: 'right', 
+  },
+  {
+    element: '.node-toolkit',
+    intro: 'Node Toolkit<br><br>[Description]',
+    position: 'right',
+    // tooltipClass: 'myTooltipClass',
+    // highlightClass: 'myHighlightClass',
+  },
+  {
+    element: '.zoom-slider',
+    intro: 'Zoom Slider<br><br>[Description]',
+    position: 'left',
+  },
+  {
+    element: '.brainstorm-node',
+    intro: 'Brainstorm node<br><br>[Description]',
+    position: 'left',
+  },
+  {
+    element: '.flex-node',
+    intro: 'Flex node<br><br>[Description]',
+    position: 'left',
+  },
+  {
+    element: '.chat-node',
+    intro: 'Search node<br><br>[Description]',
+    position: 'left',
+  },
+  {
+    element: '.concept-node',
+    intro: 'Concept hierarchy node<br><br>[Description]',
+    position: 'left',
+  },
+  {
+    element: '.memo-node',
+    intro: 'Memo node<br><br>[Description]',
+    position: 'left',
+  },
+  {
+    element: '.minimap',
+    intro: 'Minimap<br><br>[Description]',
+    position: 'left',
+  },
+];
+
+const introOptions = {
+  exitOnEsc: true,
+  showStepNumbers: true,
+  hidePrev: true,
+  hideNext: true,
+  showButtons: true,
+  keyboardNavigation: true,
+  showProgress: true,
+}
 
 const NodeToolkit = (props: any) => {
 
   const reactFlowInstance = useReactFlow();
+  const [stepsEnabled, setStepsEnabled] = useState(false);
 
   const addChatNode = useCallback(
     () => {
@@ -81,8 +153,13 @@ const NodeToolkit = (props: any) => {
     console.log(el);
   }
 
+  const onExit = () => { setStepsEnabled(false) };
+
+  const startIntroJS = () => {setStepsEnabled(true)}
+
+
   return (
-    <div className="node-toolkit">
+    <div className="node-toolkit" data-intro='Hello step one!'>
       <Button onClick={scale}>Scale</Button>
       <div className='toolkit-option traveller-mode-toggle'>
         <Switch checked={props.travellerMode} onChange={props.toggleTravellerMode} size='small' />
@@ -133,7 +210,7 @@ const NodeToolkit = (props: any) => {
         </svg>
         Memo
       </div>
-      <div className='toolkit-option add-node' draggable onClick={(e) => console.log(e)}>
+      <div className='toolkit-option add-node' draggable onClick={startIntroJS}>
         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M17 18.4301H13L8.54999 21.39C7.88999 21.83 7 21.3601 7 20.5601V18.4301C4 18.4301 2 16.4301 2 13.4301V7.42999C2 4.42999 4 2.42999 7 2.42999H17C20 2.42999 22 4.42999 22 7.42999V13.4301C22 16.4301 20 18.4301 17 18.4301Z" stroke="#AAAAAA" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
         <path d="M12.0001 11.36V11.15C12.0001 10.47 12.4201 10.11 12.8401 9.82001C13.2501 9.54001 13.66 9.18002 13.66 8.52002C13.66 7.60002 12.9201 6.85999 12.0001 6.85999C11.0801 6.85999 10.3401 7.60002 10.3401 8.52002" stroke="#AAAAAA" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
@@ -142,6 +219,13 @@ const NodeToolkit = (props: any) => {
         Help
       </div>
       <Button onClick={clearCanvas} type="text" block danger>Reset Canvas</Button>
+      <Steps
+        enabled={stepsEnabled}
+        steps={steps}
+        initialStep={0}
+        onExit={onExit}
+        options={introOptions}
+      />
     </div>
   )
 }
