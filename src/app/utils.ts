db832dd879f453192c2b480632b3c64980d2a8d0
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2023-03-06 14:33:33 -0800

    enabled localstorage

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,6 +1,6 @@
 export const devFlags = {
   disableOpenAI: false,
-  disableLocalStorage: true,
+  disableLocalStorage: false,
 }
 
 export const uuid = (): string => new Date().getTime().toString(36) + Math.random().toString(36).slice(2);
